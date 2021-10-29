801e4d332ba9d199c4169018f3d25ebfd73fb899
BeomkiKim <bumki1226@gmail.com>
2021-10-29 17:22:18 +0900

    Box, Item
    박스 충돌처리, 파괴시 아이템 생성

---
+++ 잃어버린 물건 찾습니다!/Assets/2. Scrips/ObjectStatus.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectStatus : MonoBehaviour
+{
+    public int Hp = 2; //오브젝트 현재체력
+    public int MaxHp = 2; //오브젝트 Max체력
+
+    public string objectName = "Box"; //오브젝트 이름
+}
