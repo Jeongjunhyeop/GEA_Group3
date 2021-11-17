de72dfc3f79085244761ffaa999fbca1c8385dfb
BeomkiKim <bumki1226@gmail.com>
2021-11-17 22:10:07 +0900

    이펙트 임포트, 스테이지 제작 조금

--- SpaceLost/Assets/2. Scrips/Object/Collider/HitArea.cs
+++ SpaceLost/Assets/2. Scrips/Object/Collider/HitArea.cs
@@ -12,15 +12,16 @@ public class HitArea : MonoBehaviour
         StartCoroutine("Active");
         switchCollider.enabled = false;
     }
+    IEnumerator Active()
+    {
+        yield return new WaitForSeconds(0.5f);
+        switchCollider.enabled = true;
+    }
     void Damage()
     {
         transform.GetComponentInParent<ObjectCtrl>().SendMessage("Damage");
 
     }
 
-    IEnumerator Active()
-    {
-        yield return new WaitForSeconds(0.5f);
-        switchCollider.enabled = true;
-    }
+
 }
