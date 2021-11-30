d0cf2178b9bb99e890a896bab758826c2a016ee4
BeomkiKim <bumki1226@gmail.com>
2021-11-30 16:00:28 +0900

    Stage2 수정
    오브젝트 추가, 콜라이더 어색한거 수정
    폭발 콜라이더 지속시간 수정

--- SpaceLost/Assets/2. Scrips/Object/Collider/ExplosionCol.cs
+++ SpaceLost/Assets/2. Scrips/Object/Collider/ExplosionCol.cs
@@ -29,7 +29,7 @@ public class ExplosionCol : MonoBehaviour
     IEnumerator Bomb()
     {
         GetComponent<Collider>().enabled = true;
-        yield return new WaitForSecondsRealtime(3.0f);
+        yield return new WaitForSecondsRealtime(2.0f);
         Destroy(gameObject);
     }
 
