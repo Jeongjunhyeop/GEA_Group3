70f114cef78e0bcd955e48f4fab7d6155580a313
sungoo <qkrtjddn234@naver.com>
2021-12-02 05:56:20 +0900

    튜토리얼 UI 손 봄
    UI가 올라와 있는 동안은 게임 내 시간이 흐르지 않음.

--- SpaceLost/Assets/2. Scrips/UI/tutorialPanel.cs
+++ SpaceLost/Assets/2. Scrips/UI/tutorialPanel.cs
@@ -7,9 +7,13 @@ public class tutorialPanel : MonoBehaviour
     public GameObject nextPanel;
     public GameObject nowPanel;
 
-
     public void nextPage()
     {
+        if(nextPanel == null)
+        {
+            endPage();
+            return;
+        }
         nowPanel.SetActive(false);
         nextPanel.SetActive(true);
     }
@@ -17,6 +21,12 @@ public class tutorialPanel : MonoBehaviour
     public void endPage()
     {
         nowPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    private void Start()
+    {
+        Time.timeScale = 0;
     }
 
     private void Update()
